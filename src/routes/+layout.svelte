5345109ee3700cd672942f6bbdfb337babfb5b4b
Sergio Peralta <seperalta88@gmail.com>
2023-05-21 10:40:07 -0300

    Update header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,7 +11,7 @@
 	</main>
 
 	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+		<p>Sergio Peralta - visit my <a href="https://github.com/SergioPeralta22">github</a> to see more projects. &copy; 2023</p>
 	</footer>
 </div>
 
