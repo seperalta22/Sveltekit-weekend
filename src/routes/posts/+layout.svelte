ee4989b56b46e1f85d932fc43b8ef0f38302360f
Sergio Peralta <seperalta88@gmail.com>
2023-05-21 22:55:22 -0300

    Test nested routes

---
+++ src/routes/posts/+layout.svelte
@@ -0,0 +1,33 @@
+<div class="posts">
+  <aside>
+    <h4>Posts</h4>
+    <nav>
+      <ul>
+        <li>
+          <a href="/posts/1">Post 1</a>
+        </li>
+        <li>
+          <a href="/posts/2">Post 2</a>
+        </li>
+        <li>
+          <a href="/posts/3">Post 3</a>
+        </li>
+
+      </ul>
+
+      
+    </nav>
+  </aside>
+  <main>
+    <slot/>
+  </main>
+</div>
+
+
+<style>
+  .posts{
+    display: grid;
+    gap: 2rem;
+    grid-template-columns: 200px 20ch;
+  }
+</style>
--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -1,1 +1,5 @@
 <h1>Post page</h1>
+
+<p>
+  You can <a href="/posts/create">+create</a> or edit your posts here.
+</p>
